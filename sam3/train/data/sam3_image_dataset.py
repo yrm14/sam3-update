14478d2c73dce905e28865028291ed362e1a19f7
Yash Mashru <you@example.com>
2025-12-03 13:56:27 +0530

    enh:{Added code to resolve more dependencies}

--- sam3/train/data/sam3_image_dataset.py
+++ sam3/train/data/sam3_image_dataset.py
@@ -15,7 +15,6 @@ from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
 import torch
 import torch.utils.data
 import torchvision
-from decord import cpu, VideoReader
 from iopath.common.file_io import g_pathmgr
 
 from PIL import Image as PILImage
@@ -181,6 +180,7 @@ class CustomCocoDetectionAPI(VisionDataset):
     def _load_images(
         self, datapoint_id: int, img_ids_to_load: Optional[Set[int]] = None
     ) -> Tuple[List[Tuple[int, PILImage.Image]], List[Dict[str, Any]]]:
+        from decord import cpu, VideoReader
         all_images = []
         all_img_metadata = []
         for current_meta in self.coco.loadImagesFromDatapoint(datapoint_id):
